06038652aae89921842e904460f2386ff804a389
dostoc <128255905+dostoc@users.noreply.github.com>
2023-03-29 10:46:11 -0300

    g3 - 2903

---
+++ src/introduccionjava/ValidaPalabra.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package introduccionjava;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jona
+ */
+public class ValidaPalabra {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String palabrabuscada = "eureka";
+        
+        Scanner leer = new Scanner(System.in);
+        boolean verdadero = false;
+        
+        while (verdadero == false) {
+                String palabra = leer.nextLine();
+                verdadero = palabrabuscada.equals(palabra);
+                if (verdadero) { 
+                    System.out.println("La palabra introducida es correcta");
+                    verdadero = true;
+                    break;
+                 }else {
+                    System.out.println("NO VALIDA");                
+   
+        }
+        
+        
+        
+    }
+    
+}
+}
---
+++ src/introduccionjava/bucleFOR.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package introduccionjava;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jona
+ */
+public class bucleFOR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingrese 4 numeros");
+        int num1 = leer.nextInt();
+        int num2 = leer.nextInt();
+        int num3 = leer.nextInt();
+        int num4 = leer.nextInt();
+       
+        // num1
+        System.out.println("");
+        System.out.print( num1 +":  ");
+        for (int i = 0; i < num1; i++) {
+            System.out.print("*");
+        }
+        // num2
+        System.out.println("");
+        System.out.print( num2 +":  ");
+        for (int i = 0; i < num2; i++) {
+            System.out.print("*");
+        }// num3
+        System.out.println("");
+        System.out.print( num3 +":  ");
+        for (int i = 0; i < num3; i++) {
+            System.out.print("*");
+        }
+        // num4
+        System.out.println("");
+        System.out.print( num4 +":  ");
+        for (int i = 0; i < num4; i++) {
+            System.out.print("*");
+        }
+        System.out.println("");
+        
+    }
+    
+}
